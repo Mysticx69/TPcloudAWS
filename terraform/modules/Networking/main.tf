ddf4e9167a7b2342adce9bd9412edc99af3fe5ec
Antoine Sterna <sternaantoine@gmail.com>
2022-11-14 20:19:48 +0100

    (feat) Removed map_public_ip_on_launch for public subnets (security compliance)

--- terraform/modules/Networking/main.tf
+++ terraform/modules/Networking/main.tf
@@ -51,7 +51,7 @@ resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnets_cidr)
   cidr_block              = element(var.public_subnets_cidr, count.index)
   availability_zone       = element(var.availability_zones, count.index)
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "${var.environment}-${element(var.availability_zones, count.index)}-public-subnet"
