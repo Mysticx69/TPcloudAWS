5ebe9281ce5567c76cc3911f096294bf0bf67da3
Antoine Sterna <sternaantoine@gmail.com>
2022-11-15 17:04:00 +0100

    (feat) Migrated remote state and updated required version of tf and providers

--- terraform/mockinfra-env/backend.tf
+++ terraform/mockinfra-env/backend.tf
@@ -1,7 +1,19 @@
 terraform {
-  backend "s3" {
-    bucket = "mockinfraprojectcpe"
-    key    = "MockInfraProjectCpe/backup_Tfstate"
-    region = "us-east-1"
+  backend "remote" {
+    organization = "mysticx"
+
+    workspaces {
+      name = "TP-cloud-aws-5IRC"
+    }
   }
 }
+
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "mockinfraprojectcpe"
+#     key    = "MockInfraProjectCpe/backup_Tfstate"
+#     region = "us-east-1"
+#   }
+# }
--- terraform/mockinfra-env/provider.tf
+++ terraform/mockinfra-env/provider.tf
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "1.3.3"
+  required_version = "~>1.3.3"
 
   required_providers {
     aws = "~>4"
